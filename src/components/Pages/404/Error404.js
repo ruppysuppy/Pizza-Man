957f9a7fe89e02f35adb7853842cd964d2888bb8
Tapajyoti Bose <ruppysuppy@gmail.com>
2020-08-13 10:06:16 +0530

    feat(pages): added error 404 page + spinner

--- src/components/Pages/404/Error404.js
+++ src/components/Pages/404/Error404.js
@@ -1,11 +1,22 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Redirect } from 'react-router'
 
 function Error404() {
+    const [redirect, setRedirect] = useState(null)
+    setTimeout(() => setRedirect(<Redirect to="/" />), 3000)
+
     return (
-        <div>
-            <h1>
+        <div className="container mt-5 pt-2">
+            <h1 className="display-2">
                 Error 404
             </h1>
+            <h1 className="pl-1">
+                It seems like you are lost!
+            </h1>
+            <h3 className="pl-1">
+                We are sending you back home
+            </h3>
+            {redirect}
         </div>
     )
 }
