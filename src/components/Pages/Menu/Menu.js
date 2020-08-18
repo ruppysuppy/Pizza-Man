2915460b12a6810e149a9bdaca427aea02186da5
Tapajyoti Bose <ruppysuppy@gmail.com>
2020-08-18 11:54:04 +0530

    chore(menu): optimized menu (converted to functional component)

--- src/components/Pages/Menu/Menu.js
+++ src/components/Pages/Menu/Menu.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'
+import React, { useEffect } from 'react'
 import { connect } from 'react-redux'
 
 import * as actions from '../../../store/actions/actions'
@@ -6,33 +6,41 @@ import * as actions from '../../../store/actions/actions'
 import Spinner from '../../UI/Spinner/Spinner'
 import Category from './Category/Category'
 
-class Menu extends Component {
-    componentDidMount() {
-        if (this.props.menu.length === 0) {
-            this.props.initMenu()
+import style from './menu.module.css'
+
+function Menu(props) {
+    const { menu, error, isLoading, initMenu } = props
+
+    useEffect(() => {
+        if (menu.length === 0) {
+            initMenu()
         }
         window.scrollTo(0, 0);
-    }
+    }, []) // initMenu, menu may be added to avoid warning (its used as componentWillMount)
 
-    render() {
-        const display = this.props.menu.map(categoryData => <Category
-            name={categoryData.name}
-            items={categoryData.items}
-            key={categoryData.name}
-        />
-        )
+    const display = menu.map(categoryData => <Category
+        name={categoryData.name}
+        items={categoryData.items}
+        key={categoryData.name}
+    />)
 
-        return (
-            <div className="mt-5 pt-2">
-                {this.props.error ?
-                    this.props.error
-                    : this.props.isLoading ?
+    return (
+        <>
+            <div className={`container mt-5 pt-2 ${style.Body}`}>
+                <h1 className="display-6 mb-0">
+                    MENU
+                </h1>
+                <hr className="mt-1 mb-4" />
+                {error ?
+                    error
+                    : isLoading ?
                         <Spinner />
                         : display}
             </div>
-        )
-    }
+        </>
+    )
 }
+
 
 const mapStateToProps = state => ({
     menu: state.menu.menu,
