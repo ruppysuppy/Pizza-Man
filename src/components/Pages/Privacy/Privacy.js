1481d6de211d020f3595c7d74a0421f2b368ceaf
Tapajyoti Bose <ruppysuppy@gmail.com>
2020-08-22 11:32:37 +0530

    feat(pages): added privacy policy page

--- src/components/Pages/Privacy/Privacy.js
+++ src/components/Pages/Privacy/Privacy.js
@@ -13,7 +13,183 @@ function Privacy() {
                 PRIVACY POLICY
             </PageInfo>
             <div className="container my-5 py-2">
-
+                <h4>
+                    <strong>
+                        Welcome Pizza Man we recognize that privacy is important!!
+                    </strong>
+                    <hr />
+                </h4>
+                <p>
+                    This site is owned by Tap Foodworks Limited. This Privacy Policy is designed to tell you about our practices regarding collection, use, and disclosure of information that you may provide via this site. Please be sure to read this entire Privacy Policy before using or submitting information to this site.
+                </p>
+                <br />
+                <h4>
+                    <strong>
+                        Consent
+                    </strong>
+                    <hr />
+                </h4>
+                <p>
+                    By using this site, you agree to the terms of this Privacy Policy. Whenever you submit information via this site, you consent to the collection, use and disclosure of the information in accordance with this Privacy Policy.
+                </p>
+                <br />
+                <h4>
+                    <strong>
+                        Information Collected
+                    </strong>
+                    <hr />
+                </h4>
+                <p>
+                    In general, you can visit this Web site without telling us who you are or revealing any information about yourself. Our web servers collect the domain names, not the e-mail addresses, of visitors.
+                </p>
+                <p>
+                    Pizza Man may collect personal information from you including your first and last name, address, telephone and mobile number(s), email address, credit card details and any other information, when you knowingly provide us with this information. This will generally occur when you either:
+                </p>
+                <ul>
+                    <li> Visit Online ordering site to place order. </li>
+                    <li> Enter into a competition or promotion. </li>
+                    <li> Participate in a survey. </li>
+                    <li> Subscribe to our mailing list. </li>
+                    <li> Register as a WOW club member. </li>
+                    <li> Apply for a job. </li>
+                    <li> Request information regarding our Franchise system. </li>
+                    <li> Submit website feedback </li>
+                </ul>
+                <p>
+                    Where ever Pizza Man collects personal information about you from someone else, Pizza Man or the franchisee will take reasonable steps to advise you.
+                </p>
+                <br />
+                <h4>
+                    <strong>
+                        Use and Disclosure
+                    </strong>
+                    <hr />
+                </h4>
+                <p>
+                    By using this site, you agree to the terms of this Privacy Policy. Whenever you submit information via this site, you consent to the collection, use and disclosure of the information in accordance with this Privacy Policy.
+                </p>
+                <p>
+                    Except as otherwise stated, we may use information collected via this site to improve the content of our site, to customize the site to your preferences, to communicate information to you (if you have requested it), and for any other purpose specified.
+                </p>
+                <p>
+                    Pizza Man may use the personal information that you submit to restaurant and process that information in order to provide goods and services. Generally, we may use your personal information in the ways in which you would expect, including but not limited, to any of the following purposes:
+                </p>
+                <p>
+                    To allow you to participate as WOW Club member.
+                </p>
+                <p>
+                    To process any job application you may submit.
+                </p>
+                <p>
+                    To provide you with any restaurant or promotion information you may request.
+                </p>
+                <p>
+                    To process any online ordering.
+                </p>
+                <p>
+                    To determine the number of visitors to our websites and conduct reviews of our sites.
+                </p>
+                <p>
+                    To fulfill prizes, awards and purchases
+                </p>
+                <p>
+                    To respond to specific requests from visitors.
+                </p>
+                <p>
+                    To keep you informed about any changes to our websites.
+                </p>
+                <p>
+                    To conduct marketing research.
+                </p>
+                <p>
+                    And to now and again send you offers or information on products or services that we consider will be of interest to you.
+                </p>
+                <p>
+                    Note: If you wish to unsubscribe from receiving any promotional communication from us please sms DOM UNSUB at 5757575 to unregister.
+                </p>
+                <p>
+                    Further information on how we use your personal information when using our Internet ordering system.
+                </p>
+                <p>
+                    When using the Pizza Man internet ordering system additional information is collected when you order a pizza online. This information assists in the delivery to your door and to verify your credit card payment. The internet order system also stores information about your order to help you remember and re-order the same menu in future. When concluding your order via the Pizza Man internet ordering system, you will be asked if you would like to become a WOW Club member. To not become a member untick the box.
+                </p>
+                <br />
+                <h4>
+                    <strong>
+                        Credit Card Details
+                    </strong>
+                    <hr />
+                </h4>
+                <p>
+                    Pizza Man does not store any of your credit card details.
+                </p>
+                <br />
+                <h4>
+                    <strong>
+                        Third Party Disclosure
+                    </strong>
+                    <hr />
+                </h4>
+                <p>
+                    Pizza Man may share statistics and personal information between themselves and with companies that are related to Pizza Man including our international branches.Personal information that you submit to Pizza Man may be disclosed to a third party: Such as our suppliers, as mailing houses, who are employed to provide some of our services such; to meet the purpose for which it was submitted.
+                </p>
+                <ul>
+                    <li> If you have provided your express consent to share the information. </li>
+                    <li> If Pizza Man are required or authorized by law to disclose the information. </li>
+                    <li> If your personal information is collected in connection with a joint promoter, to that joint promoter for marketing and research purposes. </li>
+                    <li> If Pizza Man feel you might like to know about a third party's goods and services we may supply that personal information to that third party. (We know that many of our guests enjoy receiving these excellent offers). </li>
+                    <li> Or if you place an order via our internet order system, your details will be passed on to Verisign, who receive your credit card details for internet order payment. Verisign is a secure internet payment gateway that secures your credit card number during transmission. See: www.verisign.com for further details. </li>
+                </ul>
+                <br />
+                <h4>
+                    <strong>
+                        Access, Accuracy and Security
+                    </strong>
+                    <hr />
+                </h4>
+                <p>
+                    Pizza Man will take reasonable steps to ensure that the personal information collected is accurate, complete and up-to-date. You can access and request correction of any personal information concerning you at any time. You may also request that your personal information be deleted at any time. Any such requests should be made directly by contacting us.Pizza Man will take reasonable steps to protect personal information from misuse, loss and unauthorized access, modification or disclosure.
+                </p>
+                <br />
+                <h4>
+                    <strong>
+                        Sensitive Information
+                    </strong>
+                    <hr />
+                </h4>
+                <p>
+                    Pizza Man will not collect, use or disclose sensitive information except with your specific consent.
+                </p>
+                <br />
+                <h4>
+                    <strong>
+                        Click Stream Data
+                    </strong>
+                    <hr />
+                </h4>
+                <p>
+                    Each time you visit the Pizza Man's site our server collects some anonymous information, known as click-stream data, including the type of browser and system you are using; the address of the site you have come from and move to after your visit; the date and time of your visit; and your server's IP address. Pizza Man may collect this information for statistical purposes to find out how the websites is used and navigated, including the number of hits, the frequency and duration of visits, most popular session times. Pizza Man may use this information to evaluate and improve the Pizza Man's websites.
+                </p>
+                <br />
+                <h4>
+                    <strong>
+                        Cookies
+                    </strong>
+                    <hr />
+                </h4>
+                <p>
+                    A Cookie is a piece of information that our web server may send to your machine when you visit a Pizza Man's site. A Cookie helps us to recognize you when you re-visit our sites and to co-ordinate your access to different pages on the sites. For example, once you have placed a new pizza order you may be offered the opportunity to personalize your order so that the system remembers it for future use. A cookie will be set on your machine that enables us to recognize your computer when you return to order your favorite pizza next time. With most Internet Browsers, you can erase Cookies from your computer hard drive, block all Cookies, or receive a warning before a Cookie is stored. If you want to do this, refer to your Browser instructions or help screen to learn more. If you disable all cookies, you may not be able to take advantage of all the features of our sites.
+                </p>
+                <br />
+                <h4>
+                    <strong>
+                        Changes
+                    </strong>
+                    <hr />
+                </h4>
+                <p>
+                    If this Privacy Policy is changed, the revised policy will be posted on this site. Please check back periodically, and especially before you provide any personally identifiable information. This Privacy Policy was last updated on August 22, 2020.
+                </p>
             </div>
         </>
     )
