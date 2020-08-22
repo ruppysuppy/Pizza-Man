c0048ce0552fbe30f2b6bc79c16301e7183ce53e
Tapajyoti Bose <ruppysuppy@gmail.com>
2020-08-22 11:54:26 +0530

    feat(pages): added terms and conditions page

--- src/components/Pages/Terms/Terms.js
+++ src/components/Pages/Terms/Terms.js
@@ -13,7 +13,112 @@ function Terms() {
                 TERMS & CONDITIONS
             </PageInfo>
             <div className="container my-5 py-2">
+                <h4>
+                    <strong>
+                        Pizza Man Service Use
+                    </strong>
+                    <hr />
+                </h4>
+                <p>
+                    Any User is are hereby allowed to use the Pizza Man Service subject to acceptance of the terms and conditions and the Privacy Policy on the Pizza Man Website (“Terms”). Pizza Man retains the right to change the Terms from time to time and such modified Terms shall be immediately applicable. Every time you use the Pizza Man Service, shall constitute your acceptance of changes made to Terms. For detailed terms, please refer our website.
+                </p>
+                <br />
+                <h4>
+                    <strong>
+                        Ordering
+                    </strong>
+                    <hr />
+                </h4>
+                <p>
+                    You agree to take reasonable care when providing Pizza Man with your details and warrant that these details are accurate and complete at the time of ordering food. You also warrant that the credit or debit card details that you provide are of your own credit or debit card.
+                </p>
+                <p>
+                    Pizza Man ensures quality standards and is responsible and liable for all and any issues and cases pertaining to the quality of the food or order for eg. veg/non-veg labelling etc but not limited to this, to the User directly.
 
+                </p>
+                <p>
+                    User understands that some type of Food may be suitable for Users within certain age ranges only. It is your sole responsibility to check whether the Food you are ordering is suitable for the intended recipient.
+                </p>
+                <br />
+                <h4>
+                    <strong>
+                        Prices and Payment
+                    </strong>
+                    <hr />
+                </h4>
+                <p>
+                    All prices listed on the Pizza Man Website for Food is correct at the time of publication and is as received from the Participating Restaurant. While Pizza Man takes great care to keep them up to date, the final price charged to you may change at the time of delivery ,based on the latest menu and prices. Pizza Man reserves the right to alter the menu of Food available for sale and to delete and remove from listing the menu of Food and Food Delivery options, if any.
+                </p>
+                <p>
+                    User agrees that in case of change in price, Pizza Man or its employees will not be liable to him in any manner.
+                </p>
+                <p>
+                    The total price for Food and Food Delivery including all other charges, taxes, costs, if any, will be displayed on the Pizza Man Website when you place your order. Full payment must be made for all the particulars mentioned in the order.
+                </p>
+                <p>
+                    If you choose online payment, you shall ensure that online payment mode is secured, your debit/credit card details will be encrypted to prevent the possibility of someone being able to read them as they are sent over the internet. Your credit card company may also conduct necessary security checks to confirm about your identification before making any such payment.
+                </p>
+                <br />
+                <h4>
+                    <strong>
+                        Delivery
+                    </strong>
+                    <hr />
+                </h4>
+                <p>
+                    Delivery period quoted at the time of ordering are approximate only and may vary. Food will be delivered to the address as intimated by you while ordering.
+                </p>
+                <p>
+                    Pizza Man will make every effort to deliver within the time stated; however, Pizza Man will not be liable for any loss caused to you by late delivery. If the Food is not delivered within the estimated delivery time quoted, User may contact Pizza Man by email and Pizza Man will try to ensure that you receive your order as quickly as reasonably possible.
+                </p>
+                <p>
+                    In case of a late delivery, the delivery charge will neither be voided nor refunded by Pizza Man.
+                </p>
+                <p>
+                    If you fail to accept delivery of Food at the time they are ready for delivery or Pizza Man is unable to deliver at the nominated time due to your failure to provide appropriate instructions or authorizations, then the Food shall be deemed to have been delivered to you and all risk and responsibility in relation to such Food shall pass to you. Any storage, insurance and other costs which Pizza Man incur as a result of the inability to deliver shall be your responsibility and you shall indemnify Pizza Man in full for such cost.
+                </p>
+                <p>
+                    You must ensure that at the time of delivery of Food adequate arrangements, including access where necessary, are in place for the safe delivery of the Food. Pizza Man cannot be held liable for any damage, cost or expense incurred to such Food as a result of a failure to provide adequate access or arrangements for delivery.
+                </p>
+                <br />
+                <h4>
+                    <strong>
+                        Refund Policy
+                    </strong>
+                    <hr />
+                </h4>
+                <p>
+                    A refund request usually takes 7-10 business days to reflect in customer’s account depending on the bank/wallet used for payment.
+                </p>
+                <br />
+                <h4>
+                    <strong>
+                        Personal Information and Privacy
+                    </strong>
+                    <hr />
+                </h4>
+                <p>
+                    User understands and acknowledges that by choosing Pizza Man you have allowed Pizza Man to use your personal information.
+                </p>
+                <p>
+                    User understands, acknowledges and agrees that although Pizza Man provides appropriate firewalls and protections, the Pizza Man Service is not hack proof. Data pilferage due to unauthorized hacking, virus attacks, technical issues is possible.
+                </p>
+                <p>
+                    In case Pizza Man is required to disclose your personal information in order to assist the Government Authority or in adherence to the Court Order or to protect the interest of the Pizza Man Service and/or any particular user(s), Pizza Man will disclose it without obtaining prior permission from you. You authorize us to disclose your personal information.
+                </p>
+                <p>
+                    Your use of this site means that we have your consent to send periodic communication regarding offers, alerts and updates. This communication can be through SMS, emails or any other communication tool.
+                </p>
+                <br />
+                <h4>
+                    <strong>
+                        Ownership of Intellectual Property
+                    </strong>
+                    <hr />
+                </h4>
+                <p>
+                    All intellectual property rights of the Pizza Man, including but not limited to copyright, logos, names, trademarks, service marks, design, text, sound recordings, images, links, concepts and themes are exclusively owned by the Tap FoodWorks Ltd. Any reproduction, transmission, publication, performance, broadcast, alteration, license, hyperlink, creation of derivative works or other use in whole or in part in any manner is strictly prohibited.
+                </p>
             </div>
         </>
     )
